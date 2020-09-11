2c6809ec3110b61551eac607b274f458e46a49cc
Md Jamil Uddin <azad1031@gmail.com>
2020-09-11 10:04:34 +0200

    Create JamilShop base project for Publishing Azure

---
+++ ConferencePlanner/WebMVC/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WebMVC.Models;
+
+namespace WebMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        //Hosted web API REST Service base url  
+        string Baseurl = "http://localhost:64108";
+        public async Task<ActionResult> Index()
+        {
+            List<Speaker> SpkInfo = new List<Speaker>();
+
+            using (var client = new HttpClient())
+            {
+                //Passing service base url  
+                client.BaseAddress = new Uri(Baseurl);
+
+                client.DefaultRequestHeaders.Clear();
+                //Define request data format  
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //Sending request to find web api REST service resource GetAllSpeakers using HttpClient  
+                HttpResponseMessage Res = await client.GetAsync("api/Speakers/GetAllSpeakers");
+
+                //Checking the response is successful or not which is sent using HttpClient  
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api   
+                    var SpkResponse = Res.Content.ReadAsStringAsync().Result;
+
+                    //Deserializing the response recieved from web api and storing into the Speaker list  
+                    SpkInfo = JsonConvert.DeserializeObject<List<Speaker>>(SpkResponse);
+
+                }
+                //returning the Speaker list to view  
+                return View(SpkInfo);
+            }
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ ConferencePlanner/WebMVC/Models/Speaker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebMVC.Models
+{
+    public class Speaker
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Bio { get; set; }
+
+        public virtual string WebSite { get; set; }
+    }
+}
