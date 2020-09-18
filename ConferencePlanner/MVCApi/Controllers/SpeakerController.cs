69ed8b08a2d906207f23a76e4bda9b76ea567b65
Md Jamil Uddin <azad1031@gmail.com>
2020-09-18 16:41:30 +0200

    Error POST part

---
+++ ConferencePlanner/MVCApi/Controllers/SpeakerController.cs
@@ -0,0 +1,73 @@
+﻿using MVCApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCApi.Controllers
+{
+    public class SpeakerController : Controller
+    {
+        // GET: Speaker
+        public ActionResult Index()
+        {
+            IEnumerable<SpeakerViewModel> speakers = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:63589/");
+                //HTTP GET
+                var responseTask = client.GetAsync("speaker");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<SpeakerViewModel>>();
+                    readTask.Wait();
+
+                    speakers = readTask.Result;
+                }
+                else
+                {
+
+
+                    speakers = Enumerable.Empty<SpeakerViewModel>();
+
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+            }
+            return View(speakers);
+        }
+
+        public ActionResult create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult create(SpeakerViewModel speaker)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:63589/speaker");
+
+                //HTTP POST
+                var postTask = client.PostAsJsonAsync<SpeakerViewModel>("speaker", speaker);
+                postTask.Wait();
+
+                var result = postTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+
+            ModelState.AddModelError(string.Empty, "Server Error. Please contact administrator.");
+
+            return View(speaker);
+        }
+    }
+}
---
+++ ConferencePlanner/MVCApi/Models/SpeakerViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCApi.Models
+{
+    public class SpeakerViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Bio { get; set; }
+
+        public virtual string WebSite { get; set; }
+    }
+}
